ebe1788d87a78d6f27bc05e284c7edd99652b5a0
Julian Wöhrer <julian.woehrer@gmail.com>
2023-05-19 21:57:16 +0200

    first php

--- www/index.php
+++ www/index.php
@@ -67,7 +67,7 @@
     <script>
         $(document).ready(function() {
             // Tabellen sortierbar machen
-            $("#myTable").tablesorter({
+            $("#buys").tablesorter({
                 theme: 'default',
                 widgets: ['zebra'],
                 headerTemplate: '{content}{icon}',
@@ -83,82 +83,87 @@
             });
         });
     </script>
+    <script>
+        $(document).ready(function() {
+            // Tabellen sortierbar machen
+            $("#trades").tablesorter({
+                theme: 'default',
+                widgets: ['zebra'],
+                headerTemplate: '{content}{icon}',
+                headers: { 
+                    // Pfeilsymbole für die sortierten Spalten hinzufügen
+                    0: { sorter: false },
+                    1: { sorter: 'text' },
+                    2: { sorter: 'text' },
+                    3: { sorter: 'digit' },
+                    4: { sorter: 'digit' },
+                    5: { sorter: 'text' }
+                }
+            });
+        });
+    </script>
+    <script>
+        $(document).ready(function() {
+            // Tabellen sortierbar machen
+            $("#sells").tablesorter({
+                theme: 'default',
+                widgets: ['zebra'],
+                headerTemplate: '{content}{icon}',
+                headers: { 
+                    // Pfeilsymbole für die sortierten Spalten hinzufügen
+                    0: { sorter: false },
+                    1: { sorter: 'text' },
+                    2: { sorter: 'text' },
+                    3: { sorter: 'digit' },
+                    4: { sorter: 'digit' },
+                    5: { sorter: 'text' }
+                }
+            });
+        });
+    </script>
+    <script>
+        function aktualisiereUhrzeit() {
+            var aktuelleZeit = new Date();
+            var stunden = aktuelleZeit.getHours();
+            var minuten = aktuelleZeit.getMinutes();
+            var sekunden = aktuelleZeit.getSeconds();
+
+            // Führende Nullen hinzufügen
+            stunden = (stunden < 10 ? "0" : "") + stunden;
+            minuten = (minuten < 10 ? "0" : "") + minuten;
+            sekunden = (sekunden < 10 ? "0" : "") + sekunden;
+
+            var uhrzeitAnzeige = stunden + ":" + minuten + ":" + sekunden;
+
+            // Uhrzeit in das Element mit der ID "uhrzeit" einfügen
+            document.getElementById("uhrzeit").innerHTML = uhrzeitAnzeige;
+
+            // Funktion alle 1 Sekunde aufrufen
+            setTimeout(aktualisiereUhrzeit, 1000);
+        }
+        
+        // Function to reload included files every 5 seconds
+        function reloadIncludes() {
+            $("#includedContent").load("buys.php");
+            $("#includedTrades").load("trades.php");
+        }
+
+        // After the document has loaded, initialize the functions
+        $(document).ready(function() {
+            aktualisiereUhrzeit();
+            setInterval(reloadIncludes, 5000); // Reload included files every 5 seconds
+        });
+    </script>
 </head>
 <body>
     <h1>Aktuelles Datum und Uhrzeit</h1>
+    <p>Datum: <?php echo date('d.m.Y'); ?></p>
+    <p>Uhrzeit: <span id="uhrzeit"></span></p>
 
     <?php
-        // Aktuelles Datum und Uhrzeit ermitteln
-        $aktuellesDatum = date('d.m.Y');
-        $aktuelleUhrzeit = date('H:i:s');
-
-        // Ausgabe des aktuellen Datum und Uhrzeit
-        echo "<p>Datum: $aktuellesDatum</p>";
-        echo "<p>Uhrzeit: $aktuelleUhrzeit</p>";
-
-        // Datenbankverbindung herstellen
-        $servername = '172.30.0.2';
-        $username = 'root';
-        $password = 'root';
-        $database = 'sqlalchemy';
-
-        $conn = new mysqli($servername, $username, $password, $database);
-
-        // Überprüfen der Verbindung
-        if ($conn->connect_error) {
-            die("Verbindung fehlgeschlagen: " . $conn->connect_error);
-        }
-
-        // SQL-Abfrage ausführen
-        $sql = "SELECT * FROM Buys WHERE status = 'NEW'";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            echo "<p>Buys</p>";
-
-            // Tabellenkopf ausgeben
-            echo "<table id='myTable'>";
-            echo "<thead>";
-            echo "<tr>";
-            echo "<th>ID</th>";
-            echo "<th>Symbol</th>";
-            echo "<th>transactTime</th>";
-            echo "<th>Price</th>";
-            echo "<th>Qty</th>";
-            echo "<th>Kind</th>";
-            echo "</tr>";
-            echo "</thead>";
-            echo "<tbody>";
-
-            // Datensätze ausgeben
-            while ($row = $result->fetch_assoc()) {
-
-                $epochTimeInSeconds = intval($row["transactTime"]/1000);
-
-                // Datumsformat festlegen (z.B. "d.m.Y H:i:s")
-                $dateFormat = "d.m.Y H:i:s";
-
-                // Epoch-Zeit in Datumsformat umwandeln
-                $convertedDate = date($dateFormat, $epochTimeInSeconds);
-
-                echo "<tr>";
-                echo "<td>" . $row["trade_id"] . "</td>";
-                echo "<td>" . $row["symbol"] . "</td>";
-                echo "<td>" . $convertedDate . "</td>";
-                echo "<td>" . $row["price"] . "</td>";
-                echo "<td>" . $row["origQty"] . "</td>";
-                echo "<td>" . $row["kind"] . "</td>";
-                echo "</tr>";
-            }
-
-            echo "</tbody>";
-            echo "</table>";
-        } else {
-            echo "Keine Ergebnisse gefunden.";
-        }
-
-        // Datenbankverbindung schließen
-        $conn->close();
+        include('buys.php');
+        include('trades.php');
+        include('sells.php');
     ?>
 
     <p>
