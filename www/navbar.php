aa7a41190cba5df2701870e863ffd02ec8cbd675
Julian Wöhrer <julian.woehrer@gmail.com>
2023-05-22 19:55:30 +0200

    insert extern trade

---
+++ www/navbar.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <div class="navbar">
+        <a href="#">Startseite</a>
+        <a href="insert_trade.php">Insert extern Trade</a>
+        <a href="#">Dienstleistungen</a>
+        <a href="info.php">Info</a>
+    </div>
+</body>
+</html>
